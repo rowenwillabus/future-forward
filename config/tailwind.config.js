b748d42467e12eb077a95e413871e838045ab0de
Triston Thompson <tris.thomo@gmail.com>
2024-02-20 17:01:53 -0400

    Add prelogin layout and alerts to application layout page

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -8,6 +8,20 @@ module.exports = {
     './app/views/**/*.{erb,haml,html,slim}',
     './node_modules/flowbite/**/*.js'
   ],
+  safelist: [
+    {
+      pattern: /text-(red|green|blue)-(100|200|300|400|500|700|900)/,
+      variants: ['lg', 'hover', 'focus', 'lg:hover'],
+    },
+    {
+      pattern: /bg-(red|green|blue)-(100|200|300|500|900)/,
+      variants: ['lg', 'hover', 'focus', 'lg:hover'],
+    },
+    {
+      pattern: /border-(red|green|blue)-(100|200|300|500|900)/,
+      variants: ['lg', 'hover', 'focus', 'lg:hover']
+    }
+  ],
   theme: {
     extend: {
       fontFamily: {
