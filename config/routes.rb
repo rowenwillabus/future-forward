bae3b74faefb472e5b3148961c472035fcbf29de
Triston Thompson <tris.thomo@gmail.com>
2024-02-20 16:42:43 -0400

    Generate devise views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  Rails.application.routes.draw do
+    devise_for :users, controllers: {
+      sessions: 'users/sessions',
+      registrations: 'users/registrations', 
+      passwords: 'users/passwords'
+    }
+  end
 end
