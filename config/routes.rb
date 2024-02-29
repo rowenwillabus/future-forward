aaf71afcf40a83b5bf58c269b0e7cccdaa128daa
Rowen Willabus <rowen-dev@intellectstorm.com>
2024-02-29 19:29:38 -0400

    Merge style

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   get 'home/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'home#index'
@@ -12,17 +11,4 @@ Rails.application.routes.draw do
   }
   resources :electors
   resources :lists
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  Rails.application.routes.draw do
-    resources :electors
-    resources :lists
-    devise_for :users, controllers: {
-      sessions: 'users/sessions',
-      registrations: 'users/registrations',
-      passwords: 'users/passwords'
-    }
-  end
 end
