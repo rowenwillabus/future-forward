1beacb0ea37ab8d0830bca809445598f6d2e1b01
Rowen Willabus <rowen-dev@intellectstorm.com>
2024-03-08 00:07:28 -0400

    Set root

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'home/index'
 
   # Defines the root path route ("/")
-  root 'home#index'
+  root 'electors#index'
 
   devise_for :users, controllers: {
     sessions: 'users/sessions',
