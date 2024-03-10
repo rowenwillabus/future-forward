77d1d37b6acb03ea742125e3bbb37801e76f5e77
Rowen Willabus <rowen-dev@intellectstorm.com>
2024-03-10 16:37:22 -0400

    Disable registration and forgot password

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :rememberable, :validatable
   has_many :responses
 end
